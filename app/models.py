a1ca80d138ed3b13b48ae7e228f84d1a472ed4c2
unknown <ricluper@espol.edu.ec>
2024-08-16 19:39:18 -0500

    modelos finales y serializers

--- app/models.py
+++ app/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+class Usuario(models.Model):
+    nickname = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    
+    class Meta:
+        db_table = 'usuario'
+
 class Isla(models.Model):
     nombre = models.CharField(max_length=30)
     calificacion_tres = models.IntegerField()
     calificacion_dos = models.IntegerField()
     calificacion_uno = models.IntegerField()
-
+    descripcion = models.CharField(max_length=300)
+    imagen = models.CharField(max_length=500)
+    
     class Meta:
         db_table = 'isla'
 
@@ -17,11 +26,11 @@ class TipoNegocio(models.Model):
 
 class Negocio(models.Model):
     nombre = models.CharField(max_length=30)
-    servicios = models.CharField(max_length=100)
+    servicios = models.CharField(max_length=200)
     hora_inicio = models.TimeField()
     hora_fin = models.TimeField()
-    direccion = models.CharField(max_length=50)
-    imagen = models.CharField(max_length=300)
+    direccion = models.CharField(max_length=100)
+    imagen = models.CharField(max_length=500)
     tipo_negocio = models.ForeignKey(TipoNegocio, on_delete=models.CASCADE)
     isla = models.ForeignKey(Isla, on_delete=models.CASCADE)
 
@@ -30,9 +39,10 @@ class Negocio(models.Model):
 
 class Resena(models.Model):
     descripcion = models.CharField(max_length=200)
-    fecha = models.DateField()
-    hora = models.TimeField()
+    fecha = models.DateField(auto_now=True)
+    hora = models.TimeField(auto_now=True)
     negocio = models.ForeignKey(Negocio, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'resena'
@@ -41,7 +51,7 @@ class Biodiversidad(models.Model):
     nombre_comun = models.CharField(max_length=30)
     nombre_cientifico = models.CharField(max_length=50)
     dato = models.CharField(max_length=200)
-    imagen = models.CharField(max_length=300)
+    imagen = models.CharField(max_length=500)
 
     class Meta:
         db_table = 'biodiversidad'
@@ -59,3 +69,50 @@ class Flora(Biodiversidad):
 
     class Meta:
         db_table = 'flora'
+
+class Actividad(models.Model):
+    nombre = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=100)
+    hora_inicio = models.TimeField()
+    hora_fin = models.TimeField()
+    edad = models.IntegerField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    isla = models.ForeignKey(Isla, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'actividad'
+
+class GuiasTuristicos(models.Model):
+    nombre = models.CharField(max_length=20)
+    edad = models.IntegerField()
+    telefono = models.CharField(max_length=10)
+    imagen = models.CharField(max_length=500)
+    isla = models.ForeignKey(Isla, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'guias_turisticos'
+
+class Ecosistema(models.Model):
+    nombre = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=200)
+    imagen = models.CharField(max_length=500)
+    class Meta:
+        db_table = 'ecosistema'
+
+class Comentario(models.Model):
+    comentario = models.CharField(max_length=200)
+    fecha = models.DateField(auto_now=True)
+    hora = models.TimeField(auto_now=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'comentario'
+
+class Calificacion(models.Model):
+    voto = models.BooleanField(default = False)
+    puntuacion = models.IntegerField()
+    isla = models.ForeignKey(Isla, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'calificacion'
--- app/serializer.py
+++ app/serializer.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from app.models import *
 
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ['id', 'nickname']
+
 class IslaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Isla
@@ -20,6 +25,52 @@ class NegocioSerializer(serializers.ModelSerializer):
 
 class ResenaSerializer(serializers.ModelSerializer):
     negocio = NegocioSerializer(many=False, read_only=True)
+    usuario = UsuarioSerializer(many=False, read_only=True)
     class Meta:
         model = Resena
+        fields = "__all__"
+
+class BiodiversidadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Biodiversidad
+        fields = "__all__"
+
+class FaunaSerializer(BiodiversidadSerializer):
+    class Meta:
+        model = Fauna
+        fields = "__all__"
+
+class FloraSerializer(BiodiversidadSerializer):
+    class Meta:
+        model = Flora
+        fields = "__all__"
+
+class ActividadSerializer(serializers.ModelSerializer):
+    isla = IslaSerializer(many=False, read_only=True)
+    class Meta:
+        model = Actividad
+        fields = "__all__"
+
+class GuiasTuristicosSerializer(serializers.ModelSerializer):
+    isla = IslaSerializer(many=False, read_only=True)
+    class Meta:
+        model = GuiasTuristicos
+        fields = "__all__"
+
+class EcosistemaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ecosistema
+        fields = "__all__"
+
+class ComentarioSerializer(serializers.ModelSerializer):
+    usuario = UsuarioSerializer(many=False, read_only=True)
+    class Meta:
+        model = Comentario
+        fields = "__all__"
+
+class CalificacionSerializer(serializers.ModelSerializer):
+    isla = IslaSerializer(many=False, read_only=True)
+    usuario = UsuarioSerializer(many=False, read_only=True)
+    class Meta:
+        model = Calificacion
         fields = "__all__"
